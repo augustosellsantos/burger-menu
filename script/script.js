2b159ae5e7b785889afe7f868ea0a57c0574d23d
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-07-18 16:52:27 -0300

    finalizado projeto burger menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
 const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger');
-const close = document.querySelector('.close');
-const active = document.querySelector('#burger');
+const close1 = document.querySelector('.close');
 
-active.addEventListener('click', () => {
-    close.classList.add('active')
+burger.addEventListener('click', () => {
+    menu.classList.remove('active');
+    close1.classList.remove('active');
+    burger.classList.add('active')
+});
+close1.addEventListener('click', () => {
+    menu.classList.add('active');
+    close1.classList.add('active');
+    burger.classList.remove('active');
 });
